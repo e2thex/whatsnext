0c55f56807d8fe5cb284b39b572e2855d7ded4ef
Erik Summerfield <e2thex@eriks-mbp.lan>
2025-04-29 13:57:24 -0500

    trying to fix login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -43,7 +43,7 @@ export default function Login() {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: 'github',
         options: {
-          redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/`,
+          redirectTo: `${window.location.origin}/`,
         },
       })
 
@@ -63,7 +63,7 @@ export default function Login() {
       const { error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
         options: {
-          redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/`,
+          redirectTo: `${window.location.origin}/`,
         },
       })
 
